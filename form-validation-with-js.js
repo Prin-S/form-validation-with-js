c6b99307c42eb17a917c31474bd5ee0c1bd1189f
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-08-29 22:33:28 +0700

    Added/modified functions. Added CSS for input:valid.
    Added validation functions for country, postcode, password, and confirm password. Modified validation function for email. Modified function for the Submit button. Moved function invocations outside submitForm() so that input:invalid applies to all fields when the page first loads.

--- form-validation-with-js.js
+++ form-validation-with-js.js
@@ -1,20 +1,37 @@
 const form = document.querySelector('#form');
 
-form.addEventListener('submit', submitForm);
-
 function submitForm(event) {
     // Make the setCustomValidity() message appear when the form is submitted, but the information is missing.
-    checkEmail();
-
-    // Make the setCustomValidity() message appear the first time the form is submitted, but the information is missing.
-    email.reportValidity()
+    confirmPassword.reportValidity();
+    password.reportValidity();
+    postcode.reportValidity();
+    country.reportValidity();
+    email.reportValidity();
 
     event.preventDefault();
 }
 
-const atSignRegex = /@/g;
-const emailRegex = /^[a-zA-Z0-9._%±]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/; // From https://support.boldsign.com/kb/article/15962/how-to-create-regular-expressions-regex-for-email-address-validation
+form.addEventListener('submit', submitForm);
+
 const email = document.querySelector('#email');
+const atSignRegex = /@/g;
+const TLDRegex = /.[a-zA-Z]{2,}$/;
+const emailRegex = /^[a-zA-Z0-9._%±]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/; // From https://support.boldsign.com/kb/article/15962/how-to-create-regular-expressions-regex-for-email-address-validation
+
+const country = document.querySelector('#country');
+const countryRegex = /^[a-zA-Z]+$/;
+
+const postcode = document.querySelector('#postcode');
+const postcodeRegex = /^[a-zA-Z0-9\s-]+$/;
+
+const password = document.querySelector('#password');
+const passwordSpaceRegex = /\s/;
+const passwordLowercaseRegex = /[a-z]/;
+const passwordUppercaseRegex = /[A-Z]/;
+const passwordNumbersRegex = /[0-9]/;
+const passwordSpecialCharsRegex = /[^(a-zA-Z0-9\s)]/;
+
+const confirmPassword = document.querySelector('#confirm-password');
 
 function checkEmail() {
     if (!email.value) {
@@ -27,13 +44,96 @@ function checkEmail() {
         email.setCustomValidity("At least one period must come after the @ sign.");
     } else if ((email.value.indexOf('.') - email.value.indexOf('@') == 1) || email.value.at(-1) == '.') {
         email.setCustomValidity("Email must contain a full domain name.");
+    } else if (!email.value.match(TLDRegex)) {
+        email.setCustomValidity("Email TLD has to be at least two characters.");
     } else if (!email.value.match(emailRegex)) {
-        email.setCustomValidity("Invalid character(s) in the email.");
+        email.setCustomValidity("Email contains invalid character(s).");
     } else {
         email.setCustomValidity("");
+    }
+}
+
+function checkCountry() {
+    if (!country.value) {
+        country.setCustomValidity("Country cannot be empty.");
+    } else if (country.value.length < 2) {
+        country.setCustomValidity("Country must be at least two characters.");
+    } else if (!country.value.match(countryRegex)) {
+        country.setCustomValidity("Country can only contain letters.");
+    } else {
+        country.setCustomValidity("");
+    }
+}
+
+function checkPostcode() {
+    if (!postcode.value) {
+        postcode.setCustomValidity("Postcode cannot be empty. Use - for no postcode.");
+    } else if ((postcode.value[0] == '-' || postcode.value[0] == ' ') && postcode.value.length > 1) {
+        postcode.setCustomValidity("Postcode cannot start with a hyphen or a space.");
+    } else if (postcode.value != '-' && postcode.value.length < 3) {
+        postcode.setCustomValidity("Postcode length must be at least three.");
+    } else if (!postcode.value.match(postcodeRegex)) {
+        postcode.setCustomValidity("Postcode can only contain letters, numbers, hyphens, and spaces.");
+    } else {
+        postcode.setCustomValidity("");
+    }
+}
+
+function checkPassword() {
+    if (!password.value) {
+        password.setCustomValidity("Password cannot be empty.");
+    } else if (password.value.match(passwordSpaceRegex)) {
+        password.setCustomValidity("Password cannot contain a space.");
+    } else if (!password.value.match(passwordLowercaseRegex)) {
+        password.setCustomValidity("Password must contain at least one lowercase letter.");
+    } else if (!password.value.match(passwordUppercaseRegex)) {
+        password.setCustomValidity("Password must contain at least one uppercase letter.");
+    } else if (!password.value.match(passwordNumbersRegex)) {
+        password.setCustomValidity("Password must contain at least one number.");
+    } else if (!password.value.match(passwordSpecialCharsRegex)) {
+        password.setCustomValidity("Password must contain at least one special character.");
+    } else if (password.value.length < 8) {
+        password.setCustomValidity("Password must be at least eight characters.");
+    } else if (password.value.length > 30) {
+        password.setCustomValidity("Password cannot be longer than 30 characters.");
+    } else {
+        password.setCustomValidity("");
+    }
+}
+
+function checkConfirmPassword() {
+    if (!confirmPassword.value) {
+        confirmPassword.setCustomValidity("Password cannot be empty.");
+    } else if (confirmPassword.value != password.value) {
+        confirmPassword.setCustomValidity("Passwords do not match.");
+    } else {
+        confirmPassword.setCustomValidity("");
     }
 }
 
 email.addEventListener('input', () => {
     checkEmail();
 });
+
+country.addEventListener('input', () => {
+    checkCountry();
+});
+
+postcode.addEventListener('input', () => {
+    checkPostcode();
+});
+
+password.addEventListener('input', () => {
+    checkPassword();
+});
+
+confirmPassword.addEventListener('input', () => {
+    checkConfirmPassword();
+});
+
+// Make input:invalid apply to all fields when the page first loads.
+checkEmail();
+checkCountry();
+checkPostcode();
+checkPassword();
+checkConfirmPassword();
